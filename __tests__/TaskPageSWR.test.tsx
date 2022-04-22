47e09b40f06a068768c12f5b945e3b8c4bdb6184
hagasewa <eiji@hagasewa.com>
2022-04-22 15:47:35 +0900

    taskPageTest修正

--- __tests__/TaskPageSWR.test.tsx
+++ __tests__/TaskPageSWR.test.tsx
@@ -9,32 +9,32 @@ import { TASK } from "../types/Types"
 import TaskPage from "../pages/task-page"
 
 
- const server = setupServer(
-   rest.get('https://jsonplaceholder.typicode.com/todos/', (req, res, ctx) => {
-     const query = req.url.searchParams
-     const _limit = query.get('_limit')
-     if (_limit === '10') {
-       return res(
-         ctx.status(200),
-         ctx.json([
-           {
-             userId: 1,
-             id: 31,
-             title: 'Static task A',
-             completed: true,
-           },
-           {
-             userId: 2,
-             id: 2,
-             title: 'Static task B',
-             completed: false,
-           },
-         ])
-       )
-     }
-   })
- )
- 
+const server = setupServer(
+  rest.get('https://jsonplaceholder.typicode.com/todos/', (req, res, ctx) => {
+    const query = req.url.searchParams
+    const _limit = query.get('_limit')
+    if (_limit === '10') {
+      return res(
+        ctx.status(200),
+        ctx.json([
+          {
+            userId: 1,
+            id: 31,
+            title: 'Static task A',
+            completed: true,
+          },
+          {
+            userId: 2,
+            id: 2,
+            title: 'Static task B',
+            completed: false,
+          },
+        ])
+      )
+    }
+  })
+)
+
 beforeAll(() => server.listen())
 afterEach(() => {
   server.resetHandlers()
@@ -43,8 +43,8 @@ afterEach(() => {
 afterAll(() => server.close())
 
 describe('Tasks page with userSWR / Success+Error ', () => {
-  let staticProps:TASK[]
-  staticProps=[
+  let staticProps: TASK[]
+  staticProps = [
     {
       userId: 1,
       id: 3,
@@ -73,7 +73,7 @@ describe('Tasks page with userSWR / Success+Error ', () => {
   it("Should render the value fetch by failed", async () => {
     server.use(
       rest.get(
-        "https://jsonplaceholder.typicode.com/comments/?_limit=10",
+        "https://jsonplaceholder.typicode.com/todos/?_limit=10",
         (rew, res, ctx) => {
           return res(
             ctx.status(400))
@@ -81,9 +81,9 @@ describe('Tasks page with userSWR / Success+Error ', () => {
       )
     )
     render(
-    <SWRConfig value={{ dedupingInterval: 0 }}>
-         <TaskPage staticTasks={staticProps} />
-    </SWRConfig>
+      <SWRConfig value={{ dedupingInterval: 0 }}>
+        <TaskPage staticTasks={staticProps} />
+      </SWRConfig>
     )
     expect(await screen.findByText("Error!")).toBeInTheDocument()
   })
